f7a5c9c379cc84cc637ca7f73fbfae9ee701203d
Nickss09 <nicoliemuller0iap@gmail.com>
2026-09-16 17:02:24 -0300

    Atualização nos icones e layout

--- src/app/genres/page.tsx
+++ src/app/genres/page.tsx
@@ -5,16 +5,17 @@ import "./page.css"
 export default function GenresPage() {
     return (
         <>
-        <head>
-            <title>Gêneros Literários</title>
-        </head>
-        <main>
+        <header>
+        <Image src="/logo.png" alt="logo" className="logo" width={170} height={170} />
 
             <h1>Gêneros <span>Literários</span></h1>
+        </header>
+        <main>
+            <h3>Agora, escolha o <span>estilo de leitura</span> que você mais gosta!</h3>
             <ul>
                 <Link href='/genres/romance'><li className="romance">Romance</li></Link>
                 <Link href='/genres/fantasia'><li className="fantasia">Fantasia</li></Link>
-                <Link href='/genres/misterio'><li className="misterio">Misterio</li></Link>
+                <Link href='/genres/misterio'><li className="misterio">Mistério</li></Link>
                 <Link href='/genres/suspense'><li className="suspense">Suspense</li></Link>
                 <Link href='/genres/terror'><li className="terror">Terror</li></Link>
                 <Link href='/genres/aventura'><li className="aventura">Aventura</li></Link>
